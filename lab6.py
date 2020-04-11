f3e2652acf8e34135ff18c38981179fa6c32949e
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2020-04-11 16:09:50 +0300

    -lab6 on regression's coefficients

--- lab6.py
+++ lab6.py
@@ -5,7 +5,7 @@ import scipy.optimize as opt
 
 
 def _mnk_coefficients(x, y):
-    beta_1 = (np.mean(x * y) - np.mean(x) * np.mean(y)) / (np.mean(x ** 2) - np.mean(x) ** 2)
+    beta_1 = (np.mean(x * y) - np.mean(x) * np.mean(y)) / (np.mean(x * x) - np.mean(x) ** 2)
     beta_0 = np.mean(y) - beta_1 * np.mean(x)
     return beta_0, beta_1
 
@@ -25,14 +25,14 @@ def _plot_regr(x, y, type):
     print('beta_1 = ' + str(np.around(beta_1, decimals=2)))
     result = opt.minimize(_fun_for_minim, [beta_0, beta_1], args=(x, y), method='SLSQP')
     coefs = result.x
-    a_1, a_2 = coefs[0], coefs[1]
+    a_0, a_1 = coefs[0], coefs[1]
     print('MNA')
+    print('a_0 = ' + str(np.around(a_0, decimals=2)))
     print('a_1 = ' + str(np.around(a_1, decimals=2)))
-    print('a_2 = ' + str(np.around(a_2, decimals=2)))
     plt.scatter(x[1:-2], y[1:-2], label='Выборка', edgecolor='navy')
     plt.plot(x, x * (2 * np.ones(len(x))) + 2 * np.ones(len(x)), label='Модель', color='blue')
     plt.plot(x, x * (beta_1 * np.ones(len(x))) + beta_0 * np.ones(len(x)), label='МHK', color='deepskyblue')
-    plt.plot(x, x * (a_2 * np.ones(len(x))) + a_1 * np.ones(len(x)), label='МHM', color='indigo')
+    plt.plot(x, x * (a_1 * np.ones(len(x))) + a_0 * np.ones(len(x)), label='МHM', color='indigo')
     plt.xlabel("x")
     plt.ylabel("y")
     plt.xlim([-1.8, 2])
@@ -43,11 +43,11 @@ def _plot_regr(x, y, type):
 
 
 if __name__ == '__main__':
-    x = np.arange(-1.8, 2.2, 0.2)
+    x = np.arange(-1.8, 2, 0.2)
     y = 2 * x + 2 * np.ones(len(x)) + np.random.normal(0, 1, size=len(x))
     types = ['Без возмущений', 'С возмущениями']
     _plot_regr(x, y, types[0])
-    y[0] = 10
-    y[-1] = -10
+    y[0] += 10
+    y[-1] -= 10
     _plot_regr(x, y, types[1])
 
