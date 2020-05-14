e005f38da5fca72d97586cecb7575f8a3bdbeb89
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2020-05-14 18:11:31 +0300

    add extra data for video

--- course_work/make_video.py
+++ course_work/make_video.py
@@ -1,14 +1,12 @@
 import os
-import numpy
 import cv2
-from PIL import Image
 
 
 def generate_video():
-    os.chdir("C:\\Users\\Daria\\PycharmProjects\\Lab1MathStat\\pictures")
-    image_folder = '.'  # убедитесь, что используете вашу папку
-    video_name = 'projections_in_area.avi'
-    os.chdir("C:\\Users\\Daria\\PycharmProjects\\Lab1MathStat\\pictures")
+    os.chdir("C:\\Users\\Daria\\PycharmProjects\\Lab1MathStat\\pictures_full")  # folder for full interval
+    image_folder = '.'
+    video_name = 'projections_in_area_full.avi'
+    os.chdir("C:\\Users\\Daria\\PycharmProjects\\Lab1MathStat\\pictures_full")
     images = [img for img in os.listdir(image_folder)
               if img.endswith(".jpg") or
               img.endswith(".jpeg") or
@@ -16,8 +14,8 @@ def generate_video():
     print(images)
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
-    video = cv2.VideoWriter(video_name, 0, 25, (width, height))
+    video = cv2.VideoWriter(video_name, 0, 25, (width, height))  # 25 - frames per second 
     for image in images:
         video.write(cv2.imread(os.path.join(image_folder, image)))
     cv2.destroyAllWindows()
-    video.release()  # выпуск сгенерированного видео
+    video.release()
